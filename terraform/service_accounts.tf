b01246edcd585bd61dedcfe7ed5ba8a29ef4910d
RajatBhola <rajat112.1997@gmail.com>
2025-08-20 11:58:52 +0200

    add IAM service enabling

--- terraform/service_accounts.tf
+++ terraform/service_accounts.tf
@@ -1,3 +1,10 @@
+resource "google_project_service" "iam_api" {
+  project = var.project_id
+  service = "iam.googleapis.com"
+
+  disable_on_destroy = false
+}
+
 resource "google_service_account" "cloudrun_sa" {
   account_id   = var.service_account.account_id
   display_name = var.service_account.display_name
